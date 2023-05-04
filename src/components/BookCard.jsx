af8c9785b7439aa2f0efbb8612f511556fdfbc30
intern21 <intern21@pop-os.localdomain>
2023-05-04 11:29:53 +0545

    added book card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const BookCard = ({imgUrl, title, price}) => {
+const BookCard = ({ imgUrl, title, price }) => {
   return (
-    <div className="book-card">
-      <Link to="/" className="book-card-info">
-        <div className="card-image-container">
-          <img src={imgUrl} />
-        </div>
+    <Link to="#" className="book-card">
+      <div className="card-image-container">
+        <img src={imgUrl} />
+      </div>
+      <div className="card-info">
         <div className="card-title">{title}</div>
         <div className="card-price">Rs. {price}</div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
