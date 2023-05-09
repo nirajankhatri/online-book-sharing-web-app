02fedb33410274e6c09ebcb5e8d7aa7d46eef466
intern21 <intern21@pop-os.localdomain>
2023-05-09 11:22:51 +0545

    added a slice for books

--- src/containers/books/glider/CardGlider.jsx
+++ src/containers/books/glider/CardGlider.jsx
@@ -10,48 +10,54 @@ import { db } from "../../../configs/firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
 import uuid from "react-uuid";
+import { fetchBooks } from "../thunks";
+import { useDispatch, useSelector } from "react-redux";
+import Spinner from "../../../components/Spinner";
 
 const CardGlider = () => {
-  const [books, setBooks] = useState([]);
-  const booksCollectionRef = collection(db, "books");
-  useEffect(() => {
-    const getBooks = async () => {
-      const data = await getDocs(booksCollectionRef);
-      setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+  let { loading, books, error } = useSelector((state) => state.books);
 
-    getBooks();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
   }, []);
 
   const bookList = books.map((book) => (
     <BookCard
       key={uuid()}
-      imgUrl={book.imgUrl}
-      title={book.title}
-      price={book.price}
+      imgUrl={book?.imgUrl}
+      title={book?.title}
+      price={book?.price}
     />
   ));
 
   return (
     <div className="glider-wrapper">
-      <div className="glider-btns">
-        <SeeMoreBtn />
-      </div>
-      <Glider
-        draggable
-        slidesToShow={2.5}
-        slidesToScroll={1}
-        // responsive={[
-        //   {
-        //     breakpoint: 864,
-        //     settings: {
-        //       slidesToShow: 3,
-        //     },
-        //   },
-        // ]}
-      >
-        {bookList}
-      </Glider>
+      {loading && <Spinner />}
+
+      {books.length > 0 ? (
+        <>
+          <div className="glider-btns">
+            <SeeMoreBtn />
+          </div>
+          <Glider
+            draggable
+            slidesToShow={2.5}
+            slidesToScroll={1}
+            // responsive={[
+            //   {
+            //     breakpoint: 864,
+            //     settings: {
+            //       slidesToShow: 3,
+            //     },
+            //   },
+            // ]}
+          >
+            {bookList}
+          </Glider>
+        </>
+      ) : ""}
     </div>
   );
 };
